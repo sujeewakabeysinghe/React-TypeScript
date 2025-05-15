c1b5a30f711b3ed7e12583d2229ef23e2cef5ea9
Sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2025-05-15 14:46:59 +0530

    CSS BEM Convention

--- my-app/src/components/InputField.tsx
+++ my-app/src/components/InputField.tsx
@@ -5,7 +5,7 @@ const InputField: React.FC = () => {
   return (
     <form className="input">
       <input type="text" placeholder="Enter a Task" className="input__box"/>
-      <button type="submit" className="input_submit">GO</button>
+      <button type="submit" className="input__submit">GO</button>
     </form>
   )
 }
