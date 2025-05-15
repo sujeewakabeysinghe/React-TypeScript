4d94cf36dce91bc935ef4c93eb86d5b0c9711ab6
Sujeewakabeysinghe <sujeewakabeysinghe@gmail.com>
2025-05-15 15:03:30 +0530

    useState

--- my-app/src/components/InputField.tsx
+++ my-app/src/components/InputField.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import "./styles.css";
 
-const InputField: React.FC = () => {
+interface Props {
+  todo: string,
+  setTodo: React.Dispatch<React.SetStateAction<string>>,
+}
+
+const InputField: React.FC<Props> = ({todo, setTodo}) => {
   return (
     <form className="input">
-      <input type="text" placeholder="Enter a Task" className="input__box"/>
+      <input type="text" placeholder="Enter a Task" className="input__box"
+      value={todo} onChange={(e)=>{setTodo(e.target.value)}}/>
       <button type="submit" className="input__submit">GO</button>
     </form>
   )
